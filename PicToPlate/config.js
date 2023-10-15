576a25c1d47eafc45c734ebb354a0bf989e3fa0a
Aarush <115517528+Arush223@users.noreply.github.com>
2023-10-14 21:39:15 -0700

    Update config.js

--- PicToPlate/config.js
+++ PicToPlate/config.js
@@ -1,4 +1,4 @@
-const pop_endpoint = 'https://api.eyepop.ai/api/v1/user/pops/91/config' //https://staging-api.eyepop.ai/api/v1/user/pops/XXX/config'
-const token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InFUTWx1V2dRLXgxS1JhUlFMWDZueSJ9.eyJlbWFpbCI6ImFhcnVzaGoxMzAwQGdtYWlsLmNvbSIsImlzcyI6Imh0dHBzOi8vZXllcG9wLnVzLmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw2NTJhZjIzYzEzNWI1MWExMzI0YWMxMjYiLCJhdWQiOlsiaHR0cHM6Ly9hcGkuZXllcG9wLmFpIiwiaHR0cHM6Ly9leWVwb3AudXMuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTY5NzMxMzM0MiwiZXhwIjoxNjk3Mzk5NzQyLCJhenAiOiJVd1VNNlgzZ3UwTGdoM3RBQ3BRMEt1NG15bFlkS2I5NSIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwifQ.qsH1BVY-b2HLNGIXmZLFFpF06BDmhGXSe09xeTUdi6hrsbJv2-bfEXLLlir_McPJym7mDoYuczO8xxvZXvjr7Pu9jM6nOh60K9FvtCCst8_zKF0ocz1QySGkNFM5-zlB_Uh-ZT314eVyAp7KZZMIXI1BYuojRH_XcwoLX324HG4CZmV9x6Ykc3gLqKUMs5h5BpE2D6htMG2k0hymu7gFfpqb6yxspWj_i2Slg6oc931J1mGoSEHQ4buk_bPnVJbAw-p7yzO-7mQBjA2oIN7XgY0R39avNLO1Fr2T5im37RpPwpe3S8ZDj4oL-hLC5zKer7MHKl3HAef49pXq8swB_g'
-const openai_token = 'TOKEN'
+const pop_endpoint = 'URL' //https://staging-api.eyepop.ai/api/v1/user/pops/XXX/config'
+const token = 'eyepopTOKEN' // get from https://app.eyepop.ai/dashboard -> API info
+const openai_token = 'TOKEN' // get from OpenAI dashboard
 var config = {}
